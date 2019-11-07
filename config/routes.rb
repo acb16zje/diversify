eee88bdcef87054afe3e3d7da53cf3423c755205
aca16nmk <nmkitala1@sheffield.ac.uk>
2019-11-07 16:35:22 +0000

    Added a register page. small adjustments yet to be made

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
 
   root to: "pages#home"
   get "/pricing", to: "pages#pricing"
-  get "/registration", to: "pages#registration"
+  get "/register", to: "pages#register"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
