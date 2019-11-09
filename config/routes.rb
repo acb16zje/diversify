31aa4e660147e44ed24242b539c77d47c4fcc7e7
aca16nmk <nmkitala1@sheffield.ac.uk>
2019-11-09 05:13:48 +0000

    Added a team page and made recommended changes to increase accesibility

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,6 @@ Rails.application.routes.draw do
   root to: "pages#home"
   get "/pricing", to: "pages#pricing"
   get "/register", to: "pages#register"
+  get "/about", to: "pages#about_us"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
