384bca1c381644d08afa0e341d0604ddc0fbb5c7
aca16nmk <nmkitala1@sheffield.ac.uk>
2019-11-12 19:05:23 +0000

    commenting and code standard check

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,6 +5,8 @@
 //= require flash_message
 //= require visibility_map
 
+// navbutton adds the change class to divs in the
+// navbar to allow their styles to change.
 function navButton(x) {
   x.classList.toggle("change");
   document.getElementById("navigation").classList.toggle('change');
