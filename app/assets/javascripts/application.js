afb3bada4acdbe8e910e03864f7d3afe7a2c4608
aca16nmk <nmkitala1@sheffield.ac.uk>
2019-11-12 18:48:43 +0000

    remodelled the navigation bar using mobile first design. still need to include the logo

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,3 +4,8 @@
 //= require bootstrap
 //= require flash_message
 //= require visibility_map
+
+function navButton(x) {
+  x.classList.toggle("change");
+  document.getElementById("navigation").classList.toggle('change');
+}
