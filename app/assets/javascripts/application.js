f5cdb1ee9528a04a60c7dedcb841eb154d0a8701
aca16nmk <nmkitala1@sheffield.ac.uk>
2019-11-19 12:04:14 +0000

    WOrked on Home and avout us page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,3 +23,39 @@ const openTab = (evt, tabName) => {
   evt.currentTarget.className += ' is-active';
 };
 
+var options = {
+  breakpoints : [
+    { changePoint: 480, slidesToShow: 1, slidesToScroll: 1 }],
+  autoplay: true,
+  autoplaySpeed: 5000,
+  navigationSwipe: true,
+  pagination: true,
+  infinite: true,
+  loop: true
+};
+
+bulmaCarousel.attach('#carousel-demo', {
+			});
+// Initialize all div with carousel class
+var carousels = bulmaCarousel.attach('#carousel-demo', options);
+
+// Loop on each carousel initialized
+for(var i = 0; i < carousels.length; i++) {
+	// Add listener to  event
+	carousels[i].on('before:show', state => {
+		console.log(state);
+	});
+}
+
+// Access to bulmaCarousel instance of an element
+var element = document.querySelector('#my-element');
+if (element && element.bulmaCarousel) {
+	// bulmaCarousel instance is available as element.bulmaCarousel
+	element.bulmaCarousel.on('before-show', function(state) {
+		console.log(state);
+	});
+
+  $('.slider-item').st(function() {
+    $(this).removeClass('show');
+  });
+}
