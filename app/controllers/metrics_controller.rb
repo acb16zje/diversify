bb044af2f30144a8b6ac63c2a9a71c633805a04c
acb16jhn <jhng1@sheffield.ac.uk>
2019-12-12 15:42:38 +0000

    fully working graph 2; more refactor on metrics

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -33,50 +33,28 @@ class MetricsController < ApplicationController
 
   def update_graph_time
     if params.has_key?(:graph_name)
-      id = "#graph-div"
-
       # decide on layout
       layout = decide_layout(params[:graph_name])
       
       #decide on Data and Grouping
-      case params[:graph_name]
-      when /by Newsletter/
-        time_data = ActiveRecord::Base.connection.execute("SELECT newsletters.title, newsletters.created_at, COUNT(newsletter_feedbacks)
-        as feedback_count FROM newsletters JOIN newsletter_feedbacks ON newsletter_feedbacks.created_at BETWEEN newsletters.created_at
-        AND newsletters.created_at+interval'7 days' GROUP BY newsletters.id")
-        data = [{title: 'Unsubscription', data: time_data.collect{|i| ["#{i['title']} #{i['created_at'].utc.strftime('%Y-%m-%d')}",i['feedback_count']]}}]
-      when /Newsletter/
-        data = [{title:'Subscription',data: NewsletterSubscription.all},{title:'Unsubscription', data: NewsletterFeedback.all}]
-        time = "created_at"
-      when /Subscription/
-        data = [{title:'Subscription',data: Ahoy::Event.where(name: "Clicked pricing link")}]
-        group = "properties -> 'type'"
-        time = "time"
-      when /Visits/
-        data = [{title:'Visit',data:Ahoy::Event.where(name: "Ran action")}]
-        group = "properties -> 'action'"
-      when /Average Time Spent/
-        data = [{title:'Time Spent',data: Ahoy::Event.where(name: "Time Spent")}]
-        group = "properties -> 'location'"
-        average = "cast(properties ->> 'time' as float)"
-      when /Referrers/
-        data = [{title:'Referrers',data:Ahoy::Visit.all}]
-        group = "referrer"
-        time = "started_at"
-      end
-
+      data, config = data_setter(params[:graph_name])
+      
       # set time constraint to graphs if exist
-      data.each_index do |i|
+      if params[:graph_name].include? "by Newsletter"
         if params[:time].length() == 2
-          data[i][:data] = data[i][:data].betweenDate(params[:time][0],params[:time][1])
+          data = data.select{|v|  DateTime.parse(params[:time][0]) < v[:created_at] && v[:created_at]  < DateTime.parse(params[:time][1]) + 1.days}
         elsif params[:time].length() == 1
-          data[i][:data] = data[i][:data].onDate(params[:time][0])
+          data = data.select{|v|  DateTime.parse(params[:time][0]) < v[:created_at] && v[:created_at] < DateTime.parse(params[:time][0]) + 1.days}
         end
+        data = [{title: 'Unsubscription', data: data.collect{|i| ["#{i['title']} #{i['created_at'].utc.strftime('%Y-%m-%d')}",i['feedback_count']]}}]
+      else 
+        data = time_constraint(data)
       end
 
       #Check if theres is still valid data, else return "No Data"
       if is_there_data?(data)
-        return_partial(id, layout, {data: data, group_by: group, time: time, average: average})
+        config[:data] = data
+        return_partial("#graph-div", layout, config)
       else
         return_partial(nil, nil, {})
       end
@@ -113,6 +91,7 @@ class MetricsController < ApplicationController
   end
 
   private
+    #Helper function to decide layout based on selected graph
     def decide_layout(option)
       case option
       when /by Date/
@@ -124,14 +103,54 @@ class MetricsController < ApplicationController
       end
     end
 
+    #loops through arrays and check if there is at least one array has data
     def is_there_data?(array)
       for data in array do 
-        if data.present?
+        if data[:data].present?
           next
         else
           return false
         end
       end
       return true
+    end
+
+    # set time_constraint to data based on the number of time constraint selected
+    def time_constraint(data)
+      data.each_index do |i|
+        if params[:time].length() == 2
+          data[i][:data] = data[i][:data].betweenDate(params[:time][0],params[:time][1])
+        elsif params[:time].length() == 1
+          data[i][:data] = data[i][:data].onDate(params[:time][0])
+        end
+      end
+      return data
+    end
+
+    def data_setter(option)
+      case params[:graph_name]
+      when /by Newsletter/
+        data = Newsletter.find_by_sql("SELECT newsletters.title, newsletters.created_at, COUNT(newsletter_feedbacks)
+        as feedback_count FROM newsletters JOIN newsletter_feedbacks
+         ON newsletter_feedbacks.created_at BETWEEN newsletters.created_at
+        AND newsletters.created_at+interval'7 days' GROUP BY newsletters.id")
+        config = {time: nil, average: nil, group_by: nil}
+      when /Newsletter/
+        data = [{title:'Subscription',data: NewsletterSubscription.all},{title:'Unsubscription', data: NewsletterFeedback.all}]
+        config = {time: "created_at", average: nil, group_by: nil}
+      when /Subscription/
+        data = [{title:'Subscription',data: Ahoy::Event.where(name: "Clicked pricing link")}]
+        config = {group_by: "properties -> 'type'", time: "time", average: nil}
+      when /Visits/
+        data = [{title:'Visit',data:Ahoy::Event.where(name: "Ran action")}]
+        config = {group_by: "properties -> 'action'", average: nil, time: nil}
+      when /Average Time Spent/
+        data = [{title:'Time Spent',data: Ahoy::Event.where(name: "Time Spent")}]
+        config = {group_by: "properties -> 'location'", average: "cast(properties ->> 'time' as float)", time: nil}
+      when /Referrers/
+        data = [{title:'Referrers',data:Ahoy::Visit.all}]
+        config = {group_by: "referrer", time: "started_at", average: nil}
+      end
+      return data,config
     end
 end
