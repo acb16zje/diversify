27c8b32ba25c9b5bad2ff6be73d7b09a30381099
juneezee <engzerjun@gmail.com>
2019-12-16 07:19:15 +0000

    RSpec: NewsletterSubscription model

--- spec/models/newsletter_subscription_spec.rb
+++ spec/models/newsletter_subscription_spec.rb
@@ -3,7 +3,7 @@
 # Table name: newsletter_subscriptions
 #
 #  id              :bigint           not null, primary key
-#  date_subscribed :date             not null
+#  date_subscribed :date             default(Mon, 16 Dec 2019), not null
 #  email           :string           not null
 #  subscribed      :boolean          default(TRUE), not null
 #  created_at      :datetime         not null
@@ -16,6 +16,26 @@
 
 require 'rails_helper'
 
-RSpec.describe NewsletterSubscription, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe NewsletterSubscription do
+  include ActiveJob::TestHelper
+
+  before { clear_enqueued_jobs }
+
+  describe '#send_newsletter' do
+    let(:subscriber) { create(:subscriber) }
+    let(:newsletter) { create(:newsletter) }
+    let(:job) { described_class.send_newsletter(newsletter) }
+
+    before { subscriber }
+
+    it 'a job is created' do
+      expect { job }.to have_enqueued_job.on_queue('mailers')
+    end
+
+    it 'newsletter is sent' do
+      expect { perform_enqueued_jobs { job } }
+        .to change { ActionMailer::Base.deliveries.size }.by(1)
+    end
+  end
+
 end
