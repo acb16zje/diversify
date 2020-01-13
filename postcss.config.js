a8e87a778c9b75da6e3617bc4e0da9cbe2cdc930
juneezee <engzerjun@gmail.com>
2020-01-13 21:49:41 +0000

    Re-enable PurgeCSS (disabled in 9f84d65e)

--- postcss.config.js
+++ postcss.config.js
@@ -1,24 +1,24 @@
 const postCssImport = require('postcss-import');
 const cssNano = require('cssnano');
-// const purgeCss = require('@fullhuman/postcss-purgecss')({
-  // content: [
-  //   './app/**/*.haml',
-  //   './app/helpers/**/*.rb',
-  //   './app/**/*.js',
-  //   './app/**/*.vue',
-  // ],
-  // defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-  // whitelist: ['img', 'has-addons', 'is-expanded'],
-  // whitelistPatterns: [/mdi/],
-  // whitelistPatternsChildren: [
-  //   /modal/, /dropdown/, /pagination/, /level/, /trix/,
-  // ],
-// });
+const purgeCss = require('@fullhuman/postcss-purgecss')({
+  content: [
+    './app/**/*.haml',
+    './app/helpers/**/*.rb',
+    './app/**/*.js',
+    './app/**/*.vue',
+  ],
+  defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+  whitelist: ['img', 'has-addons', 'is-expanded'],
+  whitelistPatterns: [/mdi/],
+  whitelistPatternsChildren: [
+    /modal/, /dropdown/, /pagination/, /level/, /trix/,
+  ],
+});
 
 module.exports = {
   plugins: [
     postCssImport,
     cssNano,
-    // purgeCss,
+    purgeCss,
   ],
 };
