30cbbf6186dae7bf4ad3fdda315db46384d65ff4
juneezee <engzerjun@gmail.com>
2020-01-14 00:54:00 +0000

    Change defaultIconComponent to Iconify

---
+++ app/assets/javascripts/components/buefy/Icon.vue
@@ -0,0 +1,19 @@
+<template>
+  <span class="iconify" :data-icon="iconifyIcon" data-width="24" data-height="24" />
+</template>
+
+<script>
+export default {
+  props: {
+    icon: {
+      type: Array,
+      required: true,
+    },
+  },
+  computed: {
+    iconifyIcon() {
+      return `${this.icon[0]}:${this.icon[1].slice(4)}`;
+    },
+  },
+};
+</script>
--- app/assets/javascripts/packs/application.js
+++ app/assets/javascripts/packs/application.js
@@ -12,13 +12,7 @@ import Buefy from 'buefy';
 
 require('@rails/ujs').start();
 require('@iconify/iconify/dist/iconify');
-require('@mdi/font/scss/materialdesignicons.scss');
 
-Vue.use(Buefy);
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-// const images = require.context('../images', true);
-// const imagePath = (name) => images(name, true);
-// require.context('../images', true);
+Vue.use(Buefy, {
+  defaultIconComponent: () => import('../components/buefy/Icon.vue')
+});
