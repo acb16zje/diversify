a113bbb02a1a9bc45fd1e98386df41ee0f64b9d5
juneezee <engzerjun@gmail.com>
2020-01-31 21:53:18 +0000

    Return the correct HTTP status for error pages

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -5,4 +5,20 @@ class ErrorsController < ApplicationController
   skip_before_action :authenticate_user!
 
   layout 'errors'
+
+  def error_403
+    render status: 403
+  end
+
+  def error_404
+    render status: 404
+  end
+
+  def error_422
+    render status: 422
+  end
+
+  def error_500
+    render status: 500
+  end
 end
