1984317c822a9a9981633ad2ff398821f48b4f26
acb16jhn <jhng1@sheffield.ac.uk>
2020-02-20 22:00:14 +0000

    Feature: Working Form for Personalities

--- app/controllers/users/settings/personalities_controller.rb
+++ app/controllers/users/settings/personalities_controller.rb
@@ -1,10 +1,40 @@
 # frozen_string_literal: true
 
+# Controller for setting user personality
 class Users::Settings::PersonalitiesController < Users::Settings::BaseController
+  before_action :prepare_user_personality, only: %i[show update]
+
   def show
     render 'users/settings/personalities'
   end
 
-  def update; end
+  def update
+    @user_p.personality = Personality.find_by(personalities_params)
+    if @user_p.save
+      update_success
+    else
+      render json: { message: 'Invalid Personality' },
+             status: :unprocessable_entity
+    end
+  end
 
+  private
+
+  def prepare_user_personality
+    @user_p =
+      if current_user.user_personality.nil?
+        UserPersonality.new(user: current_user)
+      else
+        current_user.user_personality
+      end
+  end
+
+  def update_success
+    flash[:toast] = { type: 'success', message: ['Personality Updated'] }
+    render js: "window.location = '#{settings_personality_path}'"
+  end
+
+  def personalities_params
+    params.require(:personality).permit(:mind, :energy, :nature, :tactic)
+  end
 end
