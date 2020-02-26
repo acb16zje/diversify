f27cac7cfefad2e5b6d88250693c9114c9bc4705
aca16nmk <nmkitala1@sheffield.ac.uk>
2020-02-26 14:45:48 +0000

    test: User Registration System tests

---
+++ spec/system/users/user_registration_spec.rb
@@ -0,0 +1,106 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'new Registration > User', :js, type: :system do
+  let(:user) do
+    User.create(
+      name: 'User',
+      email: 'user@email.com',
+      password: 'password',
+      admin: false
+    )
+  end
+
+  before do
+    visit new_user_registration_path
+  end
+
+  describe 'show sign up page' do
+    it do
+      expect(page).to have_content('Create your account')
+    end
+  end
+
+  describe 'sign up user' do
+    before do
+      visit new_user_registration_path
+    end
+
+    it 'can register user' do
+      fill_in 'user_email', with: 'newemail@email.com'
+      fill_in 'user_password', with: 'password'
+      click_button 'Sign up'
+      expect(page).to have_content('Introduction')
+    end
+  end
+
+  describe 'validate user' do
+    let(:email) do
+      find('#user_email').native.attribute('validationMessage')
+    end
+    let(:password) do
+      find('#user_password').native.attribute('validationMessage')
+    end
+
+    before do
+      visit new_user_registration_path
+    end
+
+    context 'when using a duplicate email address' do
+      it 'alerts user error' do
+        fill_in 'user_email', with: user.email
+        fill_in 'user_password', with: user.password
+        click_button 'Sign up'
+        expect(page).to have_content('has already been taken')
+      end
+    end
+
+    context 'when email missing @' do
+      it 'prompts user to complete email' do
+        fill_in 'user_email', with: 'admin'
+        click_button 'Sign up'
+        expect(email).to have_content('is missing an')
+      end
+    end
+
+    context 'when email incomplete after @' do
+      it 'prompt user to complete email' do
+        fill_in 'user_email', with: 'admin@'
+        click_button 'Sign up'
+        expect(email).to have_content('Please enter a part following')
+      end
+    end
+
+    context 'when email is blank' do
+      it 'prompt user to fill in email' do
+        click_button 'Sign up'
+        expect(email).to have_content('Please fill in this field')
+      end
+    end
+
+    context 'when password is blank' do
+      it 'prompt user to fill in password' do
+        fill_in 'user_email', with: 'admin@email.com'
+        click_button 'Sign up'
+        expect(password).to have_content('Please fill in this field')
+      end
+    end
+
+    context 'when password is short' do
+      it 'prompt user to use longer password' do
+        fill_in 'user_email', with: 'newuser@email.com'
+        fill_in 'user_password', with: 'pass'
+        click_button 'Sign up'
+        expect(page).to have_content('minimum is 6 characters')
+      end
+    end
+  end
+
+  describe 'can access other authentication pages' do
+    it 'redirects to sign in' do
+      click_link 'Sign in'
+      expect(page).to have_content('Sign in to Diversify')
+    end
+  end
+end
