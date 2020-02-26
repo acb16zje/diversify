7a744ea6b3102417319b9bf77e7a5184fc50ad16
aca16nmk <nmkitala1@sheffield.ac.uk>
2020-02-26 15:19:50 +0000

    test: User Password Replacement System tests

---
+++ spec/system/users/user_replace_password_spec.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'new Password > Password', :js, type: :system do
+  before do
+    visit new_user_password_path
+  end
+
+  let(:user) do
+    User.create(
+      name: 'User',
+      email: 'user@email.com',
+      password: 'password',
+      admin: false
+    )
+  end
+
+  describe 'show password reset page' do
+    it do
+      expect(page).to have_content('Reset Password')
+    end
+  end
+
+  describe 'can submit email' do
+    it do
+      fill_in 'user_email', with: user.email
+      click_button 'Send reset password instructions'
+    end
+  end
+
+  describe 'validate user email' do
+    let(:email) do
+      find('#user_email').native.attribute('validationMessage')
+    end
+
+    context 'when email missing @' do
+      it 'prompts user to complete email' do
+        fill_in 'user_email', with: 'admin'
+        click_button 'Send reset password instructions'
+        expect(email).to have_content('is missing an')
+      end
+    end
+
+    context 'when email incomplete after @' do
+      it 'prompt user to complete email' do
+        fill_in 'user_email', with: 'admin@'
+        click_button 'Send reset password instructions'
+        expect(email).to have_content('Please enter a part following')
+      end
+    end
+
+    context 'when email is blank' do
+      it 'prompt user to fill in email' do
+        click_button 'Send reset password instructions'
+        expect(email).to have_content('Please fill in this field')
+      end
+    end
+
+    context 'when email cannot be found' do
+      it 'prompt user email that has been registered' do
+        fill_in 'user_email', with: 'newemail@email.com'
+        click_button 'Send reset password instructions'
+        expect(page).to have_content('Email not found')
+      end
+    end
+  end
+end
