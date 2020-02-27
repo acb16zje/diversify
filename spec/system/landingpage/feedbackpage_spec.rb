959199c0c6b8e59456585249a604fa27869b3e69
Wonwoo Soh <wsoh2@sheffield.ac.uk>
2020-02-27 01:53:11 +0000

    Add: System test feedback page

--- spec/system/landingpage/feedbackpage_spec.rb
+++ spec/system/landingpage/feedbackpage_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'Navigations > Feedback', :js, type: :system do
   before do
-    visit Feedback_pages_path
+    visit feedback_pages_path
   end
 
   it do
@@ -12,15 +12,22 @@ describe 'Navigations > Feedback', :js, type: :system do
   describe 'submit the feedback form' do
 
     it 'choose all the options' do
-
+      choose(option: 'neutral')
+      select('Newspaper', :from => 'landing_feedback_channel').select_option
+      click_button 'Submit'
+      expect(page).to have_content('Thank you for your feedback')
     end
 
     it 'not choose emojis' do
-
+      select('Social Media', :from => 'landing_feedback_channel').select_option
+      click_button 'Submit'
+      expect(page).not_to have_content('Thank you for your feedback')
     end
 
     it 'not choose drop down' do
-
+      choose(option: 'sad')
+      click_button 'Submit'
+      expect(page).not_to have_content('Thank you for your feedback')
     end
 
   end
