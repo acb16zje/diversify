11f5c2d4012818a2c524e87d7604b9132f077a76
juneezee <engzerjun@gmail.com>
2020-03-09 12:38:51 +0000

    build: update dependencies (Week 5)

--- lib/scripts/push-coverage-report.sh
+++ lib/scripts/push-coverage-report.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+git config --global user.email 'engzerjun@gmail.com'
+git config --global user.name 'juneezee'
+git remote set-url origin https://acb16zje:StWb33dF6ygoCLxNwkoR@git.shefcompsci.org.uk/com4525-2019-20/team07/project.git
+git fetch origin coverage-report:coverage-report
+git checkout coverage-report
+
 if [[ `git status --porcelain --untracked-files=no` ]]; then
-  git config --global user.email 'engzerjun@gmail.com'
-  git config --global user.name 'juneezee'
-  git remote set-url origin https://acb16zje:StWb33dF6ygoCLxNwkoR@git.shefcompsci.org.uk/com4525-2019-20/team07/project.git
-  git fetch origin coverage-report:coverage-report
-  git checkout coverage-report
   git add coverage/
   git commit -m "docs: coverage report $(date +"%d-%b-%Y %R")"
   git push origin coverage-report
