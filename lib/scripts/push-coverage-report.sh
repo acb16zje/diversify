39d3135a9920c8b0b737bb83e797bdeba348c902
juneezee <engzerjun@gmail.com>
2020-03-23 08:40:47 +0000

    ci: fix coverage report not being pushed

--- lib/scripts/push-coverage-report.sh
+++ lib/scripts/push-coverage-report.sh
@@ -3,11 +3,13 @@
 git config --global user.email 'engzerjun@gmail.com'
 git config --global user.name 'juneezee'
 git remote set-url origin https://acb16zje:StWb33dF6ygoCLxNwkoR@git.shefcompsci.org.uk/com4525-2019-20/team07/project.git
-git fetch origin coverage-report:coverage-report
-git checkout coverage-report
 
-if [[ `git status --porcelain --untracked-files=no` ]]; then
-  git add coverage/
-  git commit -m "docs: coverage report $(date +"%d-%b-%Y %R")"
-  git push origin coverage-report
+if git show-ref --quiet refs/heads/coverage-report; then
+  git branch -D coverage-report
 fi
+
+git checkout --orphan coverage-report
+git rm -rf .
+git add coverage/
+git commit -m "docs: coverage report $(date +"%d-%b-%Y %R")"
+git push -f origin coverage-report
