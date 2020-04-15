37da50118694c7901a8a4a17240e26a4a520ffb5
aca16nmk <nmkitala1@sheffield.ac.uk>
2020-04-15 23:21:30 +0100

    test(system): add user_edit_project_spec.rb

---
+++ spec/system/projects/user_edit_project_spec.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'Show Project > Project', :js, type: :system do
+  let(:user) { create(:user) }
+  let(:category1) { create(:category) }
+  let(:project) { create(:project, user: user, category_id: category1.id) }
+  let(:project_archive) { create(:project, user: user, status: 'completed') }
+
+  before do
+    sign_in user
+    visit "projects/#{project.id}"
+  end
+
+  context 'when views project page' do
+    it 'shows project' do
+      expect(page).to have_content("Owned by: #{user.name}")
+    end
+  end
+
+  context 'when edits project' do
+    before do
+      find('a', text: 'Settings').click
+    end
+
+    it 'can access edit form' do
+      expect(page).to have_content('Project Details')
+    end
+
+    it 'can choose category' do
+      fill_in 'project_name', with: 'New Project'
+      select 'Accounting and Finance', from: 'project_category_id'
+      click_on 'Save Settings'
+      expect(page).to have_content('Project Updated')
+    end
+
+    it 'can write description' do
+      fill_in 'project_name', with: 'New Project'
+      select 'Accounting and Finance', from: 'project_category_id'
+      fill_in 'project_description', with: 'New Description'
+      click_button 'Save Settings'
+      expect(page).to have_content('Project Updated')
+    end
+
+    it 'title is missing' do
+      click_button 'Save Settings'
+      expect(page).to have_no_content('Project Updated')
+    end
+  end
+
+  context 'when project archived' do
+    before do
+      visit "projects/#{project_archive.id}"
+      find('a', text: 'Settings').click
+    end
+
+    it 'can access edit form' do
+      expect(page).to have_content('Project Details')
+    end
+
+    it 'can archive project' do
+      click_button 'Reactivate Project'
+      page.accept_alert
+      expect(page).to have_no_content('Project Activated')
+    end
+  end
+end
