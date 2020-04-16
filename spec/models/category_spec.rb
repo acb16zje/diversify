8833478de9e0663f54aa35318243b7288cfdbaad
juneezee <engzerjun@gmail.com>
2020-04-16 06:37:07 +0100

    test(model): add category_spec.rb

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: categories
+#
+#  id         :bigint           not null, primary key
+#  name       :string           default(""), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_categories_on_name  (name) UNIQUE
+#
+require 'rails_helper'
+
+describe Category, type: :model do
+  describe 'associations' do
+    it { is_expected.to have_many(:skills).dependent(:nullify) }
+    it { is_expected.to have_many(:projects).dependent(:nullify) }
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name) }
+  end
+end
--- spec/simplecov_env.rb
+++ spec/simplecov_env.rb
@@ -26,15 +26,16 @@ module SimpleCovEnv
 
     def configure_profile
       SimpleCov.configure do
-        load_profile 'rails'
+        load_profile 'test_frameworks'
 
+        add_filter %r{^/config/}
+        add_filter %r{^/db/}
         add_filter 'vendor/'
+        add_filter 'app/jobs'
         add_filter 'app/controllers/reviews_controller.rb'
         add_filter 'app/controllers/tasks_controller.rb'
         add_filter 'app/controllers/teams_controller.rb'
 
-        add_filter 'app/jobs'
-        add_filter 'app/models/category.rb'
         add_filter 'app/models/issue.rb'
         add_filter 'app/models/preference.rb'
         add_filter 'app/models/review.rb'
@@ -42,10 +43,15 @@ module SimpleCovEnv
         add_filter 'app/models/skill_level.rb'
         add_filter 'app/models/task.rb'
         add_filter 'app/models/team.rb'
-        add_filter 'lib/'
 
+        add_group "Controllers", "app/controllers"
+        add_group "Models", "app/models"
+        add_group "Mailers", "app/mailers"
+        add_group "Helpers", "app/helpers"
         add_group 'Policies', 'app/policies'
         add_group 'Validators',  'app/validators'
+
+        track_files '{app}/**/*.rb'
 
         merge_timeout 365.days
       end
