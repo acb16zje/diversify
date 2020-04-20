405a66ac7499e2d361f8066abd01e7a22f1ad960
juneezee <engzerjun@gmail.com>
2020-04-20 15:49:55 +0100

    test: add dashboard_controller_spec.rb

---
+++ spec/requests/admin/dashboard_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe Admin::DashboardController, type: :request do
+  let(:user) { create(:user) }
+  let(:admin) { create(:admin) }
+
+  describe 'GET #index' do
+    subject(:request) { get admin_dashboard_index_path }
+
+    it_behaves_like 'accessible to admin users'
+    it_behaves_like 'not accessible to non-admin users'
+  end
+end
