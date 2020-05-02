c2a5198163da46436674c39b4d4fa4f32cc3b872
juneezee <engzerjun@gmail.com>
2020-05-02 14:06:00 +0100

    ci: add spec:system job

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -38,7 +38,6 @@ RSpec.configure do |config|
   config.include Capybara::DSL # Let's us use the capybara stuff in our specs
   config.include Warden::Test::Helpers
   config.include Devise::Test::IntegrationHelpers
-  config.include Devise::Test::ControllerHelpers, type: :controller
 
   # Include custom helpers
   config.include OAuthHelper
@@ -54,41 +53,33 @@ RSpec.configure do |config|
     driven_by(:rack_test)
   end
 
-  config.before(:each, type: :system, js: true) do
-    driven_by(:selenium_chrome_headless)
+  config.before(:each, :js, type: :system) do
+    driven_by(:chrome)
   end
 end
 
 Webdrivers.install_dir = Rails.root.join('vendor', 'webdrivers')
 Webdrivers.cache_time = 86_400
 
-require 'selenium/webdriver'
 Capybara.register_driver :chrome do |app|
-  chrome_options = Selenium::WebDriver::Chrome::Options.new
-  chrome_options.add_argument('--headless') unless ENV['SHOW_CHROME']
-  chrome_options.add_argument('--no-sandbox')
-  chrome_options.add_argument('--disable-gpu')
-  chrome_options.add_argument('--disable-dev-shm-usage')
-  chrome_options.add_argument('--disable-infobars')
-  chrome_options.add_argument('--disable-extensions')
-  chrome_options.add_argument('--disable-popup-blocking')
-  chrome_options.add_argument('--window-size=1920,4320')
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument('headless') unless ENV['SHOW_CHROME']
+  options.add_argument('no-sandbox')
+  options.add_argument('disable-gpu')
+  options.add_argument('disable-dev-shm-usage')
+  options.add_argument('disable-infobars')
+  options.add_argument('disable-extensions')
+  options.add_argument('disable-popup-blocking')
+  options.add_argument('window-size=1366,768')
+  options.add_argument("user-data-dir=/tmp/chrome") if ENV['CI']
 
-  # LTSP has multiple versions of Chrome installed, so prefer Chromium
-  LTSP_BIN = '/usr/bin/chromium-browser'
-  chrome_options.binary = LTSP_BIN if File.exist?(LTSP_BIN)
-
-  # Rails takes a little time to start, so wait ~2 mins before failing
-  client = Selenium::WebDriver::Remote::Http::Default.new
-  client.read_timeout = 120 # seconds
-
-  Capybara::Selenium::Driver.new app, browser: :chrome, options: chrome_options, http_client: client
+  Capybara::Selenium::Driver.new app, browser: :chrome, options: options
 end
 
 Capybara.configure do |config|
+  config.server = :puma, { Silent: true }
   config.asset_host = 'http://localhost:3000'
   config.javascript_driver = :chrome
-  config.match = :prefer_exact
 end
 
 Shoulda::Matchers.configure do |config|
