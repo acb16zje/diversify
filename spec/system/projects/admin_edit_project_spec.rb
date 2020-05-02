65256d413e5c57126954e65aae68461e0976c3ff
Wonwoo Soh <wsoh2@sheffield.ac.uk>
2020-05-03 01:07:30 +0900

    test(system): fix test for admin edit project

--- spec/system/projects/admin_edit_project_spec.rb
+++ spec/system/projects/admin_edit_project_spec.rb
@@ -4,16 +4,20 @@ require 'rails_helper'
 
 describe 'Edit Project > Project', :js, type: :system do
   let(:admin) { create(:admin) }
-  let(:project) { create(:project, user: admin, category: create(:category), :private) }
+  let(:project) { create(:project, :private, user: admin, category: create(:category)) }
 
   before do
     sign_in admin
     visit "projects/#{project.id}"
+    find('a', text: 'Settings').click
   end
 
   context 'when project is private' do
     it 'can change to public' do
-      expect(page).to have_content("Owned by: #{user.name}")
+      page.check('project_visibility')
+      click_button 'Save Settings'
+      find('a', text: 'Settings').click
+      expect(page).to have_field('project_visibility', checked: true)
     end
   end
 end
